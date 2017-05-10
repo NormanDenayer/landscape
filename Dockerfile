339ad6d82313d0444b2560486650d88d3cd0285f
Norman Denayer <norman.denayer@netaxis.be>
2017-05-10 18:19:06 +0200

    update the config_prod

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,9 @@ WORKDIR /landscape
 RUN set -x && apk add --update postgresql-dev gcc python3-dev musl-dev
 
 RUN pip install -r requirements.txt
-RUN APP_SETTINGS="config_dev" python setup.py install
+RUN APP_SETTINGS="config_prod" python setup.py install
 
 ENV FLASK_APP landscape
 CMD ["flask", "run", "-h", "0.0.0.0", "-p", "5000"]
 EXPOSE 5000
+VOLUME /data
--- config_prod.py
+++ config_prod.py
@@ -5,12 +5,6 @@ DEBUG = False
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
 
-# Define the database - we are working with
-# SQLite for this example
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-DATABASE_CONNECT_OPTIONS = {}
-
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle
 # incoming requests using one and performing background
@@ -19,3 +13,12 @@ THREADS_PER_PAGE = 2
 
 # Secret key for signing cookies
 SECRET_KEY = "07c031a25e7d132d3e650a041a32cd72"
+
+# Define the database - we are working with
+# SQLite for this example
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join('/data/app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_CONNECT_OPTIONS = {}
+
+LOG_PATH = '/tmp/landscape.log'
+STATIC_FOLDER = os.path.join(BASE_DIR, 'static/')
